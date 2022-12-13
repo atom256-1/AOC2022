f6ed73edce45750c0e6ce798abf3c958c2f7d837
atom256-1 <pierrelucrivet.personnel@outlook.com>
2022-12-13 01:31:17 -0500

    December 6th exercise

--- December 06/question_01.py
+++ December 06/question_01.py
@@ -1,17 +1,25 @@
+from collections import deque
+
 filename = "data.txt"
 
 file = open(filename, "r")
 
-content = file.read().split("\n")
+content = list(file.read())
 
 file.close()
 
+def findMarker(inputList, length) :
+	marker = deque(inputList[:length])
 
- = 0
-
-for i in range(len(content)) :
+	for i in range(length, len(inputList)) :
+		if(len(set(marker)) == len(marker)) :
+			print(i)
+			break
+		
+		marker.popleft()
+		marker.append(inputList[i])
 	
+	return i
 
-
-print()
-print()
+findMarker(content, 4)
+findMarker(content, 14)
