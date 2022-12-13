f52e0b4dc8f03818d59d5d954c88e5d3d53035eb
atom256-1 <pierrelucrivet.personnel@outlook.com>
2022-12-13 08:56:44 -0500

    December 8th exercise

--- December 08/question_01.py
+++ December 08/question_01.py
@@ -1,17 +1,101 @@
+import numpy
+
 filename = "data.txt"
 
 file = open(filename, "r")
 
-content = file.read().split("\n")
+content = numpy.array([[int(val) for val in list(string)] for string in file.read().split("\n")])
+visible = numpy.zeros(content.shape, dtype = bool)
+visible[[0, -1], :] = True
+visible[:, [0, -1]] = True
 
 file.close()
 
 
- = 0
+# Row by row
+for i in [-1, 1] :
+	for j in range(visible.shape[0]) :
+		highest = 0
+		maxVal = max(content[j, :])
+		for k in list(range(visible.shape[1]))[::i] :
+			val = content[j, k]
+			if(val > highest) :
+				visible[j, k] = True
+				highest = val
+			if(val == maxVal) :
+				break
 
-for i in range(len(content)) :
-	
+# Column by column
+for i in [-1, 1] :
+	for j in range(visible.shape[1]) :
+		highest = 0
+		maxVal = max(content[:, j])
+		for k in list(range(visible.shape[0]))[::i] :
+			val = content[k, j]
+			if(val > highest) :
+				visible[k, j] = True
+				highest = val
+			if(val == maxVal) :
+				break
 
 
+print(numpy.count_nonzero(visible))
 print()
-print()
+
+score = numpy.zeros(content.shape, dtype = int)
+
+for i in range(visible.shape[0]) :
+	for j in range(visible.shape[1]) :
+		if(visible[i, j]) :
+			height = content[i, j]
+			visible_count = [0, 0, 0, 0]
+			
+			print(i, j)
+			
+			for k in range(i + 1, visible.shape[0]) :
+				visible_count[0] += 1
+				if(content[k, j] >= height) :
+					break
+			print(list(range(i + 1, visible.shape[0])), visible_count[0])
+			
+			for k in list(range(i))[::-1] :
+				visible_count[1] += 1
+				if(content[k, j] >= height) :
+					break
+			print(list(range(i))[::-1], visible_count[1])
+			
+			for k in range(j + 1, visible.shape[1]) :
+				visible_count[2] += 1
+				if(content[i, k] >= height) :
+					break
+			print(list(range(j + 1, visible.shape[1])), visible_count[2])
+			
+			for k in list(range(j))[::-1] :
+				visible_count[3] += 1
+				if(content[i, k] >= height) :
+					break
+			print(list(range(j))[::-1], visible_count[3])
+			print()
+			
+			score[i, j] = numpy.product(visible_count)
+
+
+print(numpy.max(score))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
